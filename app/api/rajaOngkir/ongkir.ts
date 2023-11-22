bedb4949bbedb0a6513bd02cab3adaaa2b623beb
AsrafRD <asrafraffi2019@gmail.com>
2023-11-22 21:25:06 +0700

    comment kota ongkir provinsi

--- app/api/rajaOngkir/ongkir.ts
+++ app/api/rajaOngkir/ongkir.ts
@@ -1,21 +1,21 @@
-import cors from "@/utils/cors";
-import runMiddleware from "@/utils/runMiddleware";
-import { NextApiRequest, NextApiResponse } from 'next';
+// import cors from "@/utils/cors";
+// import runMiddleware from "@/utils/runMiddleware";
+// import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    await runMiddleware(req, res, cors);
+// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+//     await runMiddleware(req, res, cors);
 
-    if (req.method === "POST") {
-        const response = await fetch("https://api.rajaongkir.com/starter/cost", {
-            method: "POST",
-            headers: {
-                key: process.env.API_KEY || '', // Menggunakan || '' untuk menghindari kunci yang mungkin undefined
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(req.body), // Mengonversi body ke string JSON
-        });
+//     if (req.method === "POST") {
+//         const response = await fetch("https://api.rajaongkir.com/starter/cost", {
+//             method: "POST",
+//             headers: {
+//                 key: process.env.API_KEY || '', // Menggunakan || '' untuk menghindari kunci yang mungkin undefined
+//                 "Content-Type": "application/json",
+//             },
+//             body: JSON.stringify(req.body), // Mengonversi body ke string JSON
+//         });
         
-        const result = await response.json();
-        res.status(200).json(result);
-    }
-}
+//         const result = await response.json();
+//         res.status(200).json(result);
+//     }
+// }
